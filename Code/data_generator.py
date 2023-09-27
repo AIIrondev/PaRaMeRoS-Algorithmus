f165fbcbe2a68ae47ebbddf67efe2ad7e6121b3a
Maximilian Gruendinger <136852512+AIMaxi@users.noreply.github.com>
2023-09-27 22:17:52 +0200

    UPDATE data_generator

--- Code/data_generator.py
+++ Code/data_generator.py
@@ -6,17 +6,18 @@
 ## import area
 import os
 import logging
-import pandas as pd
+# import pandas as pd
 import matplotlib.pyplot as plt
-
 
 ## var Area
 csv_folder = 'csv_files'
 log_folder = 'log_files'
 export_folder = 'export_folder'
 data = []
-data_leangth = []
+data_length = []
 fig, ax = plt.subplots()
+time_span = []
+time_difference = []
 
 # Konfigurieren des Loggers für die Protokollierung von Informationen
 logger = logging.getLogger('A_star.py')
@@ -43,37 +44,83 @@ if not os.path.exists(export_folder):
 ## def Area
 def extract_data(file):
     with open(file, 'r') as f:
-        f.read()
-        data_leangth.append(len(f.readlines()))
-        data.append(f)
-        return data
+        lines = f.readlines()
+        data_length.append(len(lines))
+        data.append(lines)
+        extracted_data = []
+        time_stamps = []
+        for line in lines:
+            parts = line.split(' - ')
+            date_time = parts[0].strip()
+            file_name = parts[1].strip()
+            log_level = parts[2].strip()
+            message = parts[3].strip()
+            extracted_data.append((date_time, file_name, log_level, message))
+            time_stamps.append(date_time)
+        if time_stamps:
+            cleaned_string = ''.join(filter(str.isdigit, time_stamps))
+            numeric_value = int(cleaned_string)
+            formatted_string = '.'.join(
+                [numeric_value[:4], numeric_value[4:6], numeric_value[6:8], numeric_value[8:10], numeric_value[10:12],
+                 numeric_value[12:14], numeric_value[14:]])
+            time_span.extend([min(time_stamps), max(time_stamps)])
+            time_difference.append(formatted_string)
+            print(time_difference)
+        return extracted_data
 
 
-def create_statistic(data_1, data_2, titel, ytitel, xtitel, legend_label_1):
-    ax.set_titel(titel)
-    ax.xaxis(len(data_1))
-    ax.yaxis(len(data_2))
+def create_statistic(data_1, data_2, titel, ytitel, xtitel, legend_label_1, export_file):
+    fig, ax = plt.subplots()
+    ax.grid()
+    ax.set_title(titel)
     ax.set_ylabel(ytitel)
     ax.set_xlabel(xtitel)
-    ax.legend()
-    ax.plot([data_1],[data_2], label=legend_label_1)
+    ax.plot(data_1, data_2, label=legend_label_1, color='red', marker='X', linestyle=':')
+    fig.savefig(os.path.join(export_folder, export_file))
+    plt.show()
 
 
 def export_data(data_export):
     with open(os.path.join(export_folder, 'export.csv'), 'w') as f:
-        f.write(data)
+        f.write(data_export)
         return data
 
 
 def main():
-    print("Main function")
-    # zeit auf x-achse und y-achse die Anzahl der einträge
+    logger.info("Main function")
 
-    # Datentypen(Error, Debug und Warning , info) auf x-achse und y-achse Anzahl der einträge
+    extract_data(os.path.join(log_folder, 'sys.log'))
+    # Zeit auf x-achse und y-achse die Anzahl der einträge
+    x_values = time_difference
+    create_statistic(x_values, data_length, 'Zeit auf Anzahl der Einträge', 'Anzahl der Einträge', 'Zeit',
+                     'Anzahl der Einträge', 'export_Zeit_auf_menge.png')
+    if ValueError:
+        logger.error("ValueError")
+    else:
+        logger.info("Exported data to export.png")
+    # Datentypen(Error, Debug und Warning, info) auf x-achse und y-achse Anzahl der einträge
+    create_statistic(data[2], data_length, 'Datentypen auf Anzahl der Einträge', 'Anzahl der Einträge', 'Datentypen',
+                     'Anzahl der Einträge', 'export_Datentypen_auf_menge.png')
+    if ValueError:
+        logger.error("ValueError")
+    else:
+        logger.info("Exported data to export.png")
+    # Zeit auf x-achse und y-achse Datentypen(Error, Debug und Warning, info)
+    create_statistic(time_span, data[2], 'Zeit auf Datentypen', 'Datentypen', 'Zeit', 'Datentypen',
+                     'export_Zeit_auf_Datentypen.png')
+    if ValueError:
+        logger.error("ValueError")
+    else:
+        logger.info("Exported data to export.png")
+    # Verschiedene Files auf x-achse und y-achse Anzahl der einträge
+    create_statistic(data[1], data_length, 'Verschiedene Files auf Anzahl der Einträge', 'Anzahl der Einträge',
+                     'Verschiedene Files', 'Anzahl der Einträge', 'export_Verschiedene_Files_auf_menge.png')
+    if ValueError:
+        logger.error("ValueError")
+    else:
+        logger.info("Exported data to export.png")
 
-    # Zeit auf x-achse und y-achse Datentypen(Error, Debug und Warning , info)
 
-    
 ## run Area
 if __name__ == '__main__':
     main()
