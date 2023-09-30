0e30d852401787018200f867339dfb6749635fd1
Maximilian Gruendinger <136852512+AIMaxi@users.noreply.github.com>
2023-09-30 10:26:42 +0200

    UPDATE data_generator

--- Code/data_generator.py
+++ Code/data_generator.py
@@ -1,26 +1,19 @@
-# Log file wird in einen Grafen umgewandelt und exportiert
-# Input: Log file
-# Output: Graph
-
-
-## import area
 import os
 import logging
-# import pandas as pd
 import matplotlib.pyplot as plt
+import datetime
 
-## var Area
-csv_folder = 'csv_files'
-log_folder = 'log_files'
-export_folder = 'export_folder'
-data = []
+# Verzeichnisnamen
+csv_folder = '../csv_files'
+log_folder = '../log_files'
+export_folder = '../export_folder'
+
+# Listen zur Datenspeicherung
 data_length = []
-fig, ax = plt.subplots()
-time_span = []
 time_difference = []
 
-# Konfigurieren des Loggers für die Protokollierung von Informationen
-logger = logging.getLogger('A_star.py')
+# Konfigurieren des Loggers
+logger = logging.getLogger('data_generator.py')
 logger.setLevel(logging.DEBUG)
 fh = logging.FileHandler(os.path.join(log_folder, 'sys.log'))
 fh.setLevel(logging.DEBUG)
@@ -29,7 +22,7 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 fh.setFormatter(formatter)
 logger.addHandler(fh)
 
-# Überprüfen und Erstellen der Ordner für CSV-Dateien und Log-Dateien
+# Überprüfen und Erstellen der Ordner
 if not os.path.exists(csv_folder):
     os.makedirs(csv_folder)
     logger.warning(f"Folder {csv_folder} created.")
@@ -41,87 +34,95 @@ if not os.path.exists(export_folder):
     logger.warning(f"Folder {export_folder} created.")
 
 
-## def Area
+# Funktion zum Extrahieren von Daten aus der Log-Datei
 def extract_data(file):
+    extracted_data = []
+    entries_per_day = {}
+    log_levels = []
+
     with open(file, 'r') as f:
         lines = f.readlines()
         data_length.append(len(lines))
-        data.append(lines)
-        extracted_data = []
-        time_stamps = []
+
         for line in lines:
             parts = line.split(' - ')
             date_time = parts[0].strip()
             file_name = parts[1].strip()
             log_level = parts[2].strip()
             message = parts[3].strip()
-            extracted_data.append((date_time, file_name, log_level, message))
-            print(date_time)
-            time_stamps.append(date_time)
-        if time_stamps:
-            cleaned_string_1 = ''.join(filter(str.isdigit, time_stamps[0]))
-            numeric_value_1 = cleaned_string_1
-            formatted_string = '.'.join(
-                [numeric_value_1[:4], numeric_value_1[4:6], numeric_value_1[6:8], numeric_value_1[8:10], numeric_value_1[10:12],
-                 numeric_value_1[12:14], numeric_value_1[14:]])
-            time_span.extend([min(time_stamps), max(time_stamps)])
-            time_difference.append(formatted_string)
-            print(time_difference)
-        return extracted_data
+
+            # Holen Sie das Datum aus dem Zeitstempel
+            date = date_time.split()[0]
+
+            # Zählen Sie die Anzahl der Einträge für dieses Datum
+            if date in entries_per_day:
+                entries_per_day[date] += 1
+            else:
+                entries_per_day[date] = 1
+
+            log_levels.append(log_level)
+            extracted_data.append((date_time, message))
+
+    return extracted_data, entries_per_day, log_levels, file_name
 
 
-def create_statistic(data_1, data_2, titel, ytitel, xtitel, legend_label_1, export_file):
+# Funktion zum Erstellen und Speichern von Statistiken als Grafik
+def create_statistic(data_1, data_2, title, y_label, x_label, legend_label, export_file):
     fig, ax = plt.subplots()
     ax.grid()
-    ax.set_title(titel)
-    ax.set_ylabel(ytitel)
-    ax.set_xlabel(xtitel)
-    ax.plot(data_1, data_2, label=legend_label_1, color='red', marker='X', linestyle=':')
+    ax.set_title(title)
+    ax.set_ylabel(y_label)
+    ax.set_xlabel(x_label)
+    ax.plot(data_1, data_2, label=legend_label, color='red', marker='X', linestyle=':')
     fig.savefig(os.path.join(export_folder, export_file))
     plt.show()
 
 
-def export_data(data_export):
-    with open(os.path.join(export_folder, 'export.csv'), 'w') as f:
-        f.write(data_export)
-        return data
+# Funktion zum Erstellen eines Balkendiagramms
+def create_bar_chart(data_1, data_2, title, y_label, x_label, export_file):
+    plt.bar(data_1, data_2, color='blue')
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.title(title)
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.savefig(os.path.join(export_folder, export_file))
+    plt.show()
 
 
+# Hauptfunktionsaufruf
 def main():
     logger.info("Main function")
 
-    extract_data(os.path.join(log_folder, 'sys.log'))
-    # Zeit auf x-achse und y-achse die Anzahl der einträge
-    x_values = time_difference
-    create_statistic(x_values, data_length, 'Zeit auf Anzahl der Einträge', 'Anzahl der Einträge', 'Zeit',
-                     'Anzahl der Einträge', 'export_Zeit_auf_menge.png')
-    if ValueError:
-        logger.error("ValueError")
-    else:
-        logger.info("Exported data to export_Zeit_auf_menge.png")
-    # Datentypen(Error, Debug und Warning, info) auf x-achse und y-achse Anzahl der einträge
-    create_statistic(data[2], data_length, 'Datentypen auf Anzahl der Einträge', 'Anzahl der Einträge', 'Datentypen',
-                     'Anzahl der Einträge', 'export_Datentypen_auf_menge.png')
-    if ValueError:
-        logger.error("ValueError")
-    else:
-        logger.info("Exported data to export_Datentypen_auf_menge.png")
-    # Zeit auf x-achse und y-achse Datentypen(Error, Debug und Warning, info)
-    create_statistic(time_span, data[2], 'Zeit auf Datentypen', 'Datentypen', 'Zeit', 'Datentypen',
-                     'export_Zeit_auf_Datentypen.png')
-    if ValueError:
-        logger.error("ValueError")
-    else:
-        logger.info("Exported data to export_Zeit_auf_Datentypen.png")
-    # Verschiedene Files auf x-achse und y-achse Anzahl der einträge
-    create_statistic(data[1], data_length, 'Verschiedene Files auf Anzahl der Einträge', 'Anzahl der Einträge',
-                     'Verschiedene Files', 'Anzahl der Einträge', 'export_Verschiedene_Files_auf_menge.png')
-    if ValueError:
-        logger.error("ValueError")
-    else:
-        logger.info("Exported data to export_Verschiedene_Files_auf_menge.png")
+    extracted_data, entries_per_day, log_levels, files = extract_data(os.path.join(log_folder, 'sys.log'))
+
+    # Extrahierte Daten pro Tag
+    x_values = list(entries_per_day.keys())
+    y_values = list(entries_per_day.values())
+
+    create_bar_chart(x_values, y_values, 'Log-Menge pro Tag', 'Anzahl der Einträge', 'Datum', 'export_Log_Menge_pro_Tag.png')
+    create_statistic(x_values, y_values, 'Log-Menge pro Tag', 'Anzahl der Einträge', 'Datum',
+                     'Anzahl der Einträge pro Tag', 'export_Log_Menge_pro_Tag.png')
+    logger.info("Exported data to export_Log_Menge_pro_Tag.png")
+
+    # Anzahl der Einträge nach Log-Level
+    log_level_count = [log_levels.count('DEBUG'), log_levels.count('INFO'), log_levels.count('WARNING'), log_levels.count('ERROR')]
+    log_level_labels = ['DEBUG', 'INFO', 'WARNING', 'ERROR']
+
+    create_bar_chart(log_level_labels, log_level_count, 'Anzahl der Einträge nach Log-Level', 'Anzahl der Einträge',
+                     'Log-Level', 'export_Log_Level.png')
+    logger.info("Exported data to export_Log_Level.png")
 
 
-## run Area
+    log_level_labels_1 = [files.count('A_star.py'), files.count('data_generator.py'), files.count('Algorithmus.py'), files.count('Dijkstra´s_Algorithm.py')]
+    log_level_count_1 = ['A_star.py', 'data_generator.py', 'main.py', 'test.py']
+
+    create_bar_chart(log_level_count_1, log_level_labels_1, 'Anzahl der Einträge nach Datei',
+                     'Anzahl der Einträge', 'Dateien', 'export_Datei.png')
+
+    logger.info("Exported data to export_Datei.png")
+
+
+# Ausführung der Hauptfunktion
 if __name__ == '__main__':
     main()
