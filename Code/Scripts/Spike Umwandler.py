c5dd8ecc5abfe1fc3fe937962278e23f06b6ff95
Maximilian Gruendinger <136852512+AIMaxi@users.noreply.github.com>
2023-10-01 11:53:11 +0200

    UPDATE Spike Umwandler

--- Code/Scripts/Spike Umwandler.py
+++ Code/Scripts/Spike Umwandler.py
@@ -1,6 +1,6 @@
 ''' Importe für Spike und umwandler '''
 import spike
-
+import tenserflow as tf
 
 ''' Importe für Spike'''
 motor_module = spike.Motor('A')
@@ -8,14 +8,63 @@ motor_pair = spike.MotorPair('c', 'B')
 color_sensor = spike.ColorSensor('E')	
 distance_sensor = spike.DistanceSensor('F')
 force_sensor = spike.ForceSensor('D')
-
+calibration = 0
 
 
 # def AREA
 
 def drive_forward(distance):
-    motor_pair.start_tank(100, 100)
-	
-	while distance_sensor.get_distance_cm() > distance:
-		pass
+	motor_pair.move(distance - calibration, 'cm')
+
+
+def calibrate_driving():
+	a = distance_sensor.get_distance_cm()
+	motor_pair.move(10, 'cm')
+	b = distance_sensor.get_distance_cm()
+	calibration = a - b
+	print(calibration)
+
+
+def turn_left(angle):
+	motor_pair.move(angle, 'degrees')
+
+
+def turn_right(angle):
+	motor_pair.move(angle, 'degrees')
+
+
+def drive_backward(distance):
+	motor_pair.move(distance - calibration, 'cm')
+
+
+def drive_until_obstacle(speed):
+	while distance_sensor.get_distance_cm() > 5:
+		motor_pair.start(speed, speed)
 	motor_pair.stop()
+
+
+def drive_until_color(color, speed):
+	while color_sensor.get_color() != color:
+		motor_pair.start(speed, speed)
+	motor_pair.stop()
+
+
+def switch_module(switch):
+	while switch == False:
+		if force_sensor.is_pressed():
+			switch = True
+			return switch
+
+
+def ai_control():
+	# Hier kommt der AI Code hin
+	pass
+
+
+def main():
+	# Hier kommt der Code hin
+	pass
+
+
+if __name__ == '__main__':
+	main()
