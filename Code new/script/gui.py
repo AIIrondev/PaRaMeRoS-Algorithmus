79c9721c8dfc7ef657fbe7f01cc80345d0467133
Maximilian Gruendinger <maximiliangruendinger@gmail.com>
2024-01-04 22:54:55 +0100

    Refactor GUI class and add main menu functionality

--- Code new/script/gui.py
+++ Code new/script/gui.py
@@ -7,15 +7,42 @@ import os
 base_dir = os.path.dirname(os.path.abspath(__file__))
 icon_path = os.path.join(base_dir, "..", "Bilder", "LOGO.ico")
 
+# class main -> main funkltion GUI class
 class main:
     def __init__(self):
-        self.root = tk.CTk()
-        self.root.title("Algorithm Path finder")
-        self.root.geometry("500x500")
-        self.root.resizable(False, False)
-        self.root.config(bg="white")
-        self.root.iconbitmap(icon_path)
-        self.root.mainloop()
+        self.window = tk.CTk()
+        self.window.title("Folder sorter")
+        self.window.geometry("600x400")
+        self.window.resizable(False, False)
+        self.window._set_appearance_mode("light")
+        self.window.iconbitmap(icon_path)
+        self.main_menu()
         
+    def main_menu(self):
+        self.reset_screen()
+        tk.CTkLabel(self.window, text="Welcome to the Algorithm Path finder", font=("Arial", 25), text_color="black").place(x=75, y=25)
+        tk.CTkButton(self.window, text="Create new simulation", command=self.main_programm, corner_radius=32, font=("Arial", 19)).place(x=180, y=100)
+        tk.CTkButton(self.window, text="Load simulation", command=self.main_load, corner_radius=32, font=("Arial", 19)).place(x=205, y=150)
+        tk.CTkLabel(self.window, text="Made by: @AIIronDev", font=("Arial", 10), text_color="black").place(x=240, y=360)
+        tk.CTkLabel(self.window, text="Github: https://github.com/Iron-witch/Folder-sorter", font=("Arial", 10), text_color="blue").place(x=200, y=380)
+
+    
+    def main_programm(self):
+        self.reset_screen()
+        tk.CTkLabel(self.window, text="You ", font=("Arial", 25), text_color="black").place(x=75, y=25)
+        tk.CTkCheckbutton(self.window, text="Create new simulation", command=self.main_programm, corner_radius=32, font=("Arial", 19)).place(x=180, y=100)
+        
+
+    def main_load(self):
+        self.reset_screen()
+        pass
+        
+    def reset_screen(self):
+        for widget in self.window.winfo_children():
+            widget.destroy()
+
+
+# main running area -> main function
 if __name__ == "__main__":
-    main()
+    window = main()
+    window.window.mainloop()
