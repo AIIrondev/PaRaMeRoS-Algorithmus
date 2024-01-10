abfd10a3d8815183e0686e81ed6ffb1aae1ba7ee
Maximilian Gruendinger <maximiliangruendinger@gmail.com>
2024-01-10 21:33:06 +0100

    Add load functionality to GUI

--- Code new/script/gui.py
+++ Code new/script/gui.py
@@ -71,7 +71,13 @@ class main:
         
     def main_load(self):
         self.reset_screen()
-        pass
+        tk.CTkLabel(self.window, text="Load", font=("Arial", 25), text_color="black").place(x=75, y=25)
+        tk.CTkButton(self.window, text="Select Path", font=("Arial", 16), text_color="black", command=self.get_path).place(x=75, y=75)
+        tk.CTkButton(self.window, text="Back", command=self.main_menu, corner_radius=32, font=("Arial", 19)).place(x=205, y=300)
+        tk.CTkButton(self.window, text="Load", command=self.logic_instance.load_simulation, corner_radius=32, font=("Arial", 19)).place(x=205, y=250)
+        def get_path(self):
+            self.path_load = tk2.filedialog.askdirectory(initialdir="/", title="Select Path")
+            tk.CTkLabel(self.window, text=self.path_load, font=("Arial", 16)).place(x=75, y=50)
         
     def reset_screen(self):
         for widget in self.window.winfo_children():
