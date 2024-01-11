4f05068aa9093927b235cee95fabcef68c6ba4c6
Maximilian Gruendinger <maximiliangruendinger@gmail.com>
2024-01-11 17:35:10 +0100

    Add MIT license to A_star.py and Dijkstra's Algorithmus.py

--- Code new/script/A_star.py
+++ Code new/script/A_star.py
@@ -1,3 +1,27 @@
+'''
+MIT LICENSE
+
+© 2023 Maximilian Gründinger
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including, but not limited to, the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+NOTICE: You are not a member of another FIRST LEGO LEAGUE TEAM or intend to use it for First Lego League in Germany, Austria, Swiss!
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+DISCLAIMER: THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE, AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, 
+WHETHER IN AN ACTION OF CONTRACT, TORT, OR OTHERWISE, ARISING FROM, OUT OF, OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+This license is valid only for the year 2023. After 2023, the aforementioned terms and conditions will be considered invalid. 
+Users are not authorized to copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+excluding older or custom versions from third parties.
+
+For older versions, the above legal terms and conditions will remain valid from the year 2023 onwards.
+'''
+
 import math
 import heapq
 import csv
